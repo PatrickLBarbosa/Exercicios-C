61950342355a624d3b5df5da5ffd5bf2ad6a1081
PatrickLBarbosa <patrick.lins1@live.com>
2022-09-09 16:54:35 -0300

    Exercício nome e idade

---
+++ Exercicio2/NomeIdade.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+    int idade;
+    char nome[50];
+
+    printf("Digite a idade: ");
+    scanf("%d", &idade);
+    printf("Digite o nome: ");
+    scanf("%s", nome);
+
+    printf("Nome: %s\n", nome);
+    printf("Idade: %d\n", idade);
+
+
+
+
+
+    return 0;
+}
